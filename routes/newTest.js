8a13bb98cbbb2fe2d64d4a9dccef59d0cdf4519d
ZangievM <zmar1996@gmail.com>
2018-09-21 11:45:46 +0300

    edited view and routes

--- routes/newTest.js
+++ routes/newTest.js
@@ -7,36 +7,37 @@ var launcher = require('../data/launcher')
 var fileSystem = require('../data/fileSystem')
 var model = require('../data/model')
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   launcher.getConnectedDevices()
-  .then(result=>{
-    res.render('newTest', { title: 'Express',devices:result });
-  })
-  
+    .then(result => {
+      res.render('newTest', {
+        title: 'Tester',
+        devices: result
+      });
+    })
+
 });
-router.post('/', function(req, res, next) {
+router.post('/', function (req, res, next) {
   let x = req.body.devices
-  var form = new formidable.IncomingForm({ 
+  var form = new formidable.IncomingForm({
     uploadDir: fileSystem.cachePath,
     keepExtensions: true
   });
   let devices = []
-  form.on('field', function(name, value) {
+  form.on('field', function (name, value) {
     devices.push(value)
-    
+
   });
   form.parse(req, function (err, fields, files) {
-    if(err) {
+    if (err) {
       console.log(err);
       return
-      
+
     }
     var file = files.file
     var testFile = files.testFile
-    model.createTestRuns(devices,file.path,testFile.path)
-    .then(result=>{
-      res.json(result)
-    })
+    model.createTestRuns(devices, file.path, testFile.path)
+    res.redirect('/')
   })
 })
 module.exports = router
