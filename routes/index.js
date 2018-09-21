74807afe46064822528793a03c4fc332e5ea3dba
ZangievM <zmar1996@gmail.com>
2018-09-21 20:54:41 +0300

    added reports(without drawing)

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,24 @@ var router = express.Router();
 const model = require('../data/model')
 const cachePath = require('../data/fileSystem').cachePath
 const fs = require('fs')
+const util = require('util')
+let reader = util.promisify(fs.readFile)
 
-
-/* GET users listing. */
 router.get('/', function (req, res, next) {
   let testRuns = model.getTestRuns()
   res.render('index', {
     testRuns: testRuns,
     path: cachePath
   });
+});
+router.get('/report/:id', function (req, res, next) {
+  let id = req.params.id
+  let path = cachePath+id+'\\index.html'
+  reader(path,'utf-8')
+  .then(result=>{
+    res.write(result);
+  })
+  
 });
 router.get('/apk/:id/:type', function (req, res, next) {
   let id = req.params.id
